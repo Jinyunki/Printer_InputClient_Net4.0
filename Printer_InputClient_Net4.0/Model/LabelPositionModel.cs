a2d6ae74895e628de9bed46e1f9680cd402815e4
ykjin <ghpp1449@gmail.com>
2023-09-22 17:48:31 +0900

    UI 구조 설계및 데이터 바인딩 처리 진행중

---
+++ Printer_InputClient_Net4.0/Model/LabelPositionModel.cs
@@ -0,0 +1,454 @@
+﻿using GalaSoft.MvvmLight;
+using PrintCommand;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Printer_InputClient_Net4._0.Model
+{
+    public class LabelPositionModel : ViewModelBase
+    {
+
+        public static string FILEPATH = Path.Combine(@"D:\0.DefaultFile\JinYunki\Printer_InputClient_Net4.0\Printer_InputClient_Net4.0\bin\Data", "PrintPointRecipie.xlsx");
+        public TPCLCommand tpclCommand = new TPCLCommand();
+        public ReadExcelData readExcelData = new ReadExcelData();
+
+        private string _printerName = string.Empty;
+        public string PrinterName
+        {
+            get { return _printerName; }
+            set {
+                _printerName = value;
+                RaisePropertyChanged("PrinterName");
+            }
+        }
+        private string _inputPrinterCommand;
+        public string InputPrinterCommand
+        {
+            get { return _inputPrinterCommand; }
+            set {
+                _inputPrinterCommand = value;
+                RaisePropertyChanged(nameof(InputPrinterCommand));
+            }
+        }
+
+        private string _workSheetName;
+        public string WorkSheetName
+        {
+            get { return _workSheetName; }
+            set {
+                _workSheetName = value;
+                RaisePropertyChanged(nameof(WorkSheetName));
+            }
+        }
+        //private ObservableCollection<ObservableCollection<string>> _excelTotalData = new ObservableCollection<ObservableCollection<string>>();
+        public ObservableCollection<ObservableCollection<string>> ExcelTotalData
+        {
+            get { return readExcelData.excelTotalData; }
+            set {
+                readExcelData.excelTotalData = value;
+                RaisePropertyChanged(nameof(ExcelTotalData));
+            }
+        }
+        private ObservableCollection<string> _positionCategorise = new ObservableCollection<string>();
+        public ObservableCollection<string> PositionCategorise
+        {
+            get { return _positionCategorise; }
+            set {
+                _positionCategorise = value;
+                RaisePropertyChanged(nameof(PositionCategorise));
+            }
+        }
+        private ObservableCollection<string> _positionData = new ObservableCollection<string>();
+        public ObservableCollection<string> PositionData
+        {
+            get { return _positionData; }
+            set {
+                _positionData = value;
+                RaisePropertyChanged(nameof(PositionData));
+            }
+        }
+        private string _labelSizeX = string.Empty;
+        public string LabelSizeX
+        {
+            get { return ExcelTotalData[0][1]; }
+            set {
+                ExcelTotalData[0][1] = value;
+                RaisePropertyChanged("LabelSizeX");
+            }
+        }
+
+        private string _labelSizeY = string.Empty;
+        public string LabelSizeY
+        {
+            get { return ExcelTotalData[1][1]; }
+            set {
+                ExcelTotalData[1][1] = value;
+                RaisePropertyChanged("LabelSizeY");
+            }
+        }
+
+        private string _printX = string.Empty;
+        public string PrintX
+        {
+            get { return ExcelTotalData[2][1]; }
+            set {
+                ExcelTotalData[2][1] = value;
+                RaisePropertyChanged("PrintX");
+            }
+        }
+
+        private string _printY = string.Empty;
+        public string PrintY
+        {
+            get { return ExcelTotalData[3][1]; }
+            set {
+                ExcelTotalData[3][1] = value;
+                RaisePropertyChanged("PrintY");
+            }
+        }
+
+        private string _groundX = string.Empty;
+        public string GroundX
+        {
+            get { return _groundX; }
+            set {
+                _groundX = value;
+                RaisePropertyChanged("GroundX");
+            }
+        }
+
+        private string _groundY = string.Empty;
+        public string GroundY
+        {
+            get { return _groundY; }
+            set {
+                _groundY = value;
+                RaisePropertyChanged("GroundY");
+            }
+        }
+
+        private string _factoryX = string.Empty;
+        public string FactoryX
+        {
+            get { return _factoryX; }
+            set {
+                _factoryX = value;
+                RaisePropertyChanged("FactoryX");
+            }
+        }
+
+        private string _factoryY = string.Empty;
+        public string FactoryY
+        {
+            get { return _factoryY; }
+            set {
+                _factoryY = value;
+                RaisePropertyChanged("FactoryY");
+            }
+        }
+
+        private string _carNameX = string.Empty;
+        public string CarNameX
+        {
+            get { return _carNameX; }
+            set {
+                _carNameX = value;
+                RaisePropertyChanged("CarNameX");
+            }
+        }
+
+        private string _carNameY = string.Empty;
+        public string CarNameY
+        {
+            get { return _carNameY; }
+            set {
+                _carNameY = value;
+                RaisePropertyChanged("CarNameY");
+            }
+        }
+
+        private string _deliveryX = string.Empty;
+        public string DeliveryX
+        {
+            get { return _deliveryX; }
+            set {
+                _deliveryX = value;
+                RaisePropertyChanged("DeliveryX");
+            }
+        }
+
+        private string _deliveryY = string.Empty;
+        public string DeliveryY
+        {
+            get { return _deliveryY; }
+            set {
+                _deliveryY = value;
+                RaisePropertyChanged("DeliveryY");
+            }
+        }
+
+        private string _productNumX = string.Empty;
+        public string ProductNumX
+        {
+            get { return _productNumX; }
+            set {
+                _productNumX = value;
+                RaisePropertyChanged("ProductNumX");
+            }
+        }
+
+        private string _productNumY = string.Empty;
+        public string ProductNumY
+        {
+            get { return _productNumY; }
+            set {
+                _productNumY = value;
+                RaisePropertyChanged("ProductNumY");
+            }
+        }
+
+        private string _countX = string.Empty;
+        public string CountX
+        {
+            get { return _countX; }
+            set {
+                _countX = value;
+                RaisePropertyChanged("CountX");
+            }
+        }
+
+        private string _countY = string.Empty;
+        public string CountY
+        {
+            get { return _countY; }
+            set {
+                _countY = value;
+                RaisePropertyChanged("CountY");
+            }
+        }
+
+        private string _productNameX = string.Empty;
+        public string ProductNameX
+        {
+            get { return _productNameX; }
+            set {
+                _productNameX = value;
+                RaisePropertyChanged("ProductNameX");
+            }
+        }
+
+        private string _productNameY = string.Empty;
+        public string ProductNameY
+        {
+            get { return _productNameY; }
+            set {
+                _productNameY = value;
+                RaisePropertyChanged("ProductNameY");
+            }
+        }
+
+        private string _productColorX = string.Empty;
+        public string ProductColorX
+        {
+            get { return _productColorX; }
+            set {
+                _productColorX = value;
+                RaisePropertyChanged("ProductColorX");
+            }
+        }
+
+        private string _productColorY = string.Empty;
+        public string ProductColorY
+        {
+            get { return _productColorY; }
+            set {
+                _productColorY = value;
+                RaisePropertyChanged("ProductColorY");
+            }
+        }
+
+        private string _barcodeX = string.Empty;
+        public string BarcodeX
+        {
+            get { return _barcodeX; }
+            set {
+                _barcodeX = value;
+                RaisePropertyChanged("BarcodeX");
+            }
+        }
+
+        private string _barcodeY = string.Empty;
+        public string BarcodeY
+        {
+            get { return _barcodeY; }
+            set {
+                _barcodeY = value;
+                RaisePropertyChanged("BarcodeY");
+            }
+        }
+
+        private string _deliveryDateX = string.Empty;
+        public string DeliveryDateX
+        {
+            get { return _deliveryDateX; }
+            set {
+                _deliveryDateX = value;
+                RaisePropertyChanged("DeliveryDateX");
+            }
+        }
+
+        private string _deliveryDateY = string.Empty;
+        public string DeliveryDateY
+        {
+            get { return _deliveryDateY; }
+            set {
+                _deliveryDateY = value;
+                RaisePropertyChanged("DeliveryDateY");
+            }
+        }
+
+        private string _companyX = string.Empty;
+        public string CompanyX
+        {
+            get { return _companyX; }
+            set {
+                _companyX = value;
+                RaisePropertyChanged("CompanyX");
+            }
+        }
+
+        private string _companyY = string.Empty;
+        public string CompanyY
+        {
+            get { return _companyY; }
+            set {
+                _companyY = value;
+                RaisePropertyChanged("CompanyY");
+            }
+        }
+
+        private string _factorySerialX = string.Empty;
+        public string FactorySerialX
+        {
+            get { return _factorySerialX; }
+            set {
+                _factorySerialX = value;
+                RaisePropertyChanged("FactorySerialX");
+            }
+        }
+
+        private string _factorySerialY = string.Empty;
+        public string FactorySerialY
+        {
+            get { return _factorySerialY; }
+            set {
+                _factorySerialY = value;
+                RaisePropertyChanged("FactorySerialY");
+            }
+        }
+
+        private string _lotNoX = string.Empty;
+        public string LotNoX
+        {
+            get { return _lotNoX; }
+            set {
+                _lotNoX = value;
+                RaisePropertyChanged("LotNoX");
+            }
+        }
+
+        private string _lotNoY = string.Empty;
+        public string LotNoY
+        {
+            get { return _lotNoY; }
+            set {
+                _lotNoY = value;
+                RaisePropertyChanged("LotNoY");
+            }
+        }
+
+        private string _HPCX = string.Empty;
+        public string HPCX
+        {
+            get { return _HPCX; }
+            set {
+                _HPCX = value;
+                RaisePropertyChanged("HPCX");
+            }
+        }
+        private string _HPCY = string.Empty;
+        public string HPCY
+        {
+            get { return _HPCY; }
+            set {
+                _HPCY = value;
+                RaisePropertyChanged("HPCY");
+            }
+        }
+
+        private string _issueNumX = string.Empty;
+        public string IssueNumX
+        {
+            get { return _issueNumX; }
+            set {
+                _issueNumX = value;
+                RaisePropertyChanged("IssueNumX");
+            }
+        }
+
+        private string _issueNumY = string.Empty;
+        public string IssueNumY
+        {
+            get { return _issueNumY; }
+            set {
+                _issueNumY = value;
+                RaisePropertyChanged("IssueNumY");
+            }
+        }
+
+        private string _containerX = string.Empty;
+        public string ContainerX
+        {
+            get { return _containerX; }
+            set {
+                _containerX = value;
+                RaisePropertyChanged("ContainerX");
+            }
+        }
+
+        private string _containerY = string.Empty;
+        public string ContainerY
+        {
+            get { return _containerY; }
+            set {
+                _containerY = value;
+                RaisePropertyChanged("ContainerY");
+            }
+        }
+
+        private string _bigProductNameX = string.Empty;
+        public string BigProductNameX
+        {
+            get { return _bigProductNameX; }
+            set {
+                _bigProductNameX = value;
+                RaisePropertyChanged("BigProductNameX");
+            }
+        }
+
+        private string _bigProductNameY = string.Empty;
+        public string BigProductNameY
+        {
+            get { return _bigProductNameY; }
+            set {
+                _bigProductNameY = value;
+                RaisePropertyChanged("BigProductNameY");
+            }
+        }
+
+    }
+}
--- Printer_InputClient_Net4.0/ViewModel/MainViewModel.cs
+++ Printer_InputClient_Net4.0/ViewModel/MainViewModel.cs
@@ -1,59 +1,54 @@
-using GalaSoft.MvvmLight;
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows.Input;
 using System.Windows;
 using System;
-using PrintCommand;
 using OfficeOpenXml;
 using LicenseContext = OfficeOpenXml.LicenseContext;
-using System.IO;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight.Command;
+using System.Text;
+using Printer_InputClient_Net4._0.Model;
+using CaptureCommand;
+using System.Collections.Generic;
 
 namespace Printer_InputClient_Net4._0.ViewModel
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : LabelPositionModel
     {
-
-        public TPCLCommand tpclCommand = new TPCLCommand();
-        public ReadExcelData readExcelData = new ReadExcelData();
-        private ObservableCollection<ObservableCollection<string>> testItem = new ObservableCollection<ObservableCollection<string>>();
-        public ObservableCollection<ObservableCollection<string>> TestItems
-        {
-            get { return testItem; }
-            set { testItem = value;
-                RaisePropertyChanged(nameof(TestItems));
-            }
-        }
-
-        public string filePath = System.IO.Path.Combine(@"D:\0.DefaultFile\JinYunki\Printer_InputClient_Net4.0\Printer_InputClient_Net4.0\bin\Data", "PrintPointRecipie.xlsx");
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
         public MainViewModel()
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             WinBtnEvent();
             //BtnSend = new Command(BtnSendCommand);
-            ExcelDataRecive();
+            ReadExcelDataRecive(1);
             PrinterSendTest("TestItemFirstResult");
-            SaveCommand = new Command(BtnSaveCommand);
+            //SaveCommand = new Command(BtnSaveCommand);
 
         }
-        public void ExcelDataRecive()
+        /// <summary>
+        /// 라이브러리를 통해 호출된 엑셀데이터를 받아온다
+        /// </summary>
+        /// <param name="selectSheet">엑셀 파일의 </param>
+        public void ReadExcelDataRecive(int selectSheet)
         {
-            readExcelData.ReadExcelDataList(filePath, 1);
-            testItem = readExcelData.excelTotalData;
-            for (int i = 0; i < testItem.Count; i++)
+            readExcelData.ReadExcelDataList(FILEPATH, selectSheet);
+            
+            for (int i = 0; i < ExcelTotalData.Count; i++)
             {
-                PositionCategorise.Add(testItem[i][0]);
-                PositionData.Add(testItem[i][1]);
+                PositionCategorise.Add(ExcelTotalData[i][0]);
+                PositionData.Add(ExcelTotalData[i][1]);
             }
             WorkSheetName = readExcelData.wrokSheetName;
 
         }
+        public void PrinterSendTest22(string testText)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tpclCommand._SetClearImageBuffer()); //클리어
+            builder.Append(tpclCommand._SetLabelSize(Double.Parse(LabelSizeY), Double.Parse(LabelSizeX), Double.Parse(PrintY), Double.Parse(PrintX))); // 라벨사이즈 지정
+        }
+
 
         public void PrinterSendTest(string testText)
         {
@@ -61,52 +56,14 @@ namespace Printer_InputClient_Net4._0.ViewModel
                                   Double.Parse(PositionData[2]), Double.Parse(PositionData[3]), Double.Parse(PositionData[4]), Double.Parse(PositionData[5]), testText);
         }
 
-        private string _inputPrinterCommand;
-        public string InputPrinterCommand
-        {
-            get { return _inputPrinterCommand; }
-            set {
-                _inputPrinterCommand = value;
-                RaisePropertyChanged(nameof(InputPrinterCommand));
-            }
-        }
 
-        private string _workSheetName;
-        public string WorkSheetName
-        {
-            get { return _workSheetName; }
-            set {
-                _workSheetName = value;
-                RaisePropertyChanged(nameof(WorkSheetName));
-            }
-        }
-
-        public ObservableCollection<string> ConvertObservableCollection(ObservableCollection<string> valueList)
+        public ObservableCollection<string> ConvertObservableCollection(List<string> valueList)
         {
             ObservableCollection<string> observableCollection = new ObservableCollection<string>(valueList);
 
             return observableCollection;
         }
-        private ObservableCollection<string> _positionCategorise = new ObservableCollection<string>();
-        private ObservableCollection<string> _positionData = new ObservableCollection<string>();
-        public ObservableCollection<string> PositionCategorise
-        {
-            get { return _positionCategorise; }
-            set {
-                _positionCategorise = value;
-                RaisePropertyChanged(nameof(PositionCategorise));
-            }
-        }
 
-
-        public ObservableCollection<string> PositionData
-        {
-            get { return _positionData; }
-            set {
-                _positionData = value;
-                RaisePropertyChanged(nameof(PositionData));
-            }
-        }
 
         private void BtnSendCommand(object obj)
         {
@@ -116,7 +73,7 @@ namespace Printer_InputClient_Net4._0.ViewModel
         }
         private void BtnSaveCommand(object obj)
         {
-            readExcelData.SaveExcelData(filePath, TestItems);
+            readExcelData.SaveExcelData(FILEPATH, ExcelTotalData);
         }
 
         private void GetPrint(string printerName) {
@@ -131,25 +88,12 @@ namespace Printer_InputClient_Net4._0.ViewModel
                 MessageBox.Show("연결 실패");
                 Console.WriteLine($"HOLY FUCK FUCK FUCK : {e.Message}");
             }
-
         }
 
 
         public ICommand BtnSend { get; set; }
         public ICommand SaveCommand { get; set; }
-
         
-        private string _printerName = string.Empty;
-        public string PrinterName
-        {
-            get { return _printerName; }
-            set {
-                _printerName = value;
-                RaisePropertyChanged("PrinterName");
-            }
-        }
-
-
 
         #region Window State
 
@@ -168,13 +112,21 @@ namespace Printer_InputClient_Net4._0.ViewModel
         public ICommand BtnMinmize { get; private set; }
         public ICommand BtnMaxsize { get; private set; }
         public ICommand BtnClose { get; private set; }
+        public ICommand BtnCapture { get; private set; }
 
         public void WinBtnEvent()
         {
             BtnMinmize = new RelayCommand(WinMinmize);
             BtnMaxsize = new RelayCommand(WinMaxSize);
             BtnClose = new RelayCommand(WindowClose);
+            BtnCapture = new Command(ViewCaptureCommand);
         }
+
+        private void ViewCaptureCommand(object obj)
+        {
+            ViewCapture.Capture(obj,"CaptureFiel");
+        }
+
         // Window Minimize
         private void WinMinmize()
         {
